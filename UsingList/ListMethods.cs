9f7c2d19aaf8f7ffdfacd01b5b8b692e5a62a66b
Sean <seansmaseola@gmail.com>
2023-08-01 09:17:51 +0200

    Add project files.

---
+++ UsingList/ListMethods.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructure.UsingList
+{
+    public class ListMethods
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("Enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Answer is " + findNumber(x, list));
+
+            Console.WriteLine("Sum is " + sumEven(list));
+
+            oddAverage(list);
+
+            multiOf3(list);
+
+            Console.ReadKey();
+        }
+
+        public static bool findNumber(int x, List<int> list)
+        {
+            return list.Contains(x);
+        }
+
+        public static int sumEven(List<int> list)
+        {
+            int total = 0;
+            for (int i = 0; i < list.Count(); i++)
+            {
+                if (list[i] % 2 == 0)
+                {
+                    total += list[i];
+                }
+            }
+            return total;
+        }
+
+        public static void oddAverage(List<int> list)
+        {
+            double total = 0;
+            double counter = 0;
+            for (int i = 0; i < list.Count(); i++)
+            {
+                if (list[i] % 2 != 0)
+                {
+                    total += list[i];
+                    counter++;
+                }
+            }
+            Console.WriteLine($"Average is {(total / counter)}");
+        }
+
+        public static void multiOf3(List<int> list)
+        {
+            foreach (var x in list)
+            {
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ UsingList/ListTester.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructure.UsingList
+{
+    public class ListTester
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>() { 2, 4, 6 };
+
+            Console.WriteLine("Original List");
+            disList(num);
+            Console.WriteLine("Add 7 to List");
+            num.Add(7);
+            disList(num);
+            Console.WriteLine("Insert 5 between 4 and 6");
+            num.Insert(2, 5);
+            disList(num);
+            Console.WriteLine("Add 1 at the start of the list");
+            num.Insert(0, 1);
+            disList(num);
+            Console.WriteLine("Remove second last element");
+            num.RemoveAt(num.Count - 2);
+            disList(num);
+            Console.WriteLine("Sum of the list");
+            Console.WriteLine("Answer: " + addList(num));
+
+            Console.ReadLine();
+            //I miss C# and should do it more.
+
+        }
+        public static int addList(List<int> list)
+        {
+            int ans = 0;
+            ans = list.Sum();
+            return ans;
+
+        }
+        public static void disList(List<int> list)
+        {
+            foreach (int x in list)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("*************************************************************");
+
+        }
+    }
+}
