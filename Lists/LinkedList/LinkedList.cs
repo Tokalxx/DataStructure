840b317bd14f137cf18ee5f32603a20289b76cbe
Sean <seansmaseola@gmail.com>
2023-08-17 08:52:30 +0200

    LinkedList

---
+++ Lists/LinkedList/LinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure.Lists
+{
+    internal class LinkedList
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+
+            list.AddLast("Tokyo");
+            list.AddFirst("Paris");
+            list.AddLast("New York");
+            list.AddFirst("Johansburg");
+            list.AddFirst("Hong Kong");
+            list.AddFirst("Brooklyn");
+            list.AddLast("Hidden Leaf Village");
+
+            displayList(list);
+
+            list.AddFirst("Benoni");
+            list.AddAfter(list.Find("Brooklyn"), "Midrand");
+            list.AddBefore(list.Find("Johansburg"), "Cape Town");
+
+            displayList(list);
+
+
+            Console.ReadLine();
+        }
+
+        public static void displayList(LinkedList<String> list)
+        {
+            foreach (string x in list)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("==================================");
+        }
+    }
+}
