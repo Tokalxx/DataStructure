9093a000e72287e8ab6757019402c93b67f613ff
Sean <seansmaseola@gmail.com>
2023-08-21 10:33:54 +0200

    MyLinkedList but for Generics. Storing and Displaying string data.

---
+++ Generics/MyLinkedList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace DataStructure.Generics
+{
+    public class MyLinkedList<T>
+    {
+
+
+        public class Node
+        {
+            public T Data { get; set; }
+            public Node Next { get; set; }
+            public Node Previous { get; set; }
+
+            public Node(T value)
+            {
+                this.Data = value;
+            }
+        }
+
+        private Node head;
+
+        public void addToFirst(T x)
+        {
+            Node newNode = new Node(x);
+
+            if (head == null)
+            {
+                head = newNode;
+
+            }
+            else
+            {
+                newNode.Next = head;
+                newNode.Previous = null;
+                head.Previous = newNode;
+                head = newNode;
+            }
+        }
+
+        public void addToLast(T x)
+        {
+            Node newNode = new Node(x);
+            if (head is null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node currentNode = head;
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+                currentNode.Next = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            /*
+             5 
+             */
+            Node currentNode = head;
+
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.Data);
+                currentNode = currentNode.Next;
+
+
+            }
+        }
+    }
+}
---
+++ Generics/MyLinkedListTester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace DataStructure.Generics
+{
+    internal class MyLinkedListTester
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedList<string> myLinkedList = new MyLinkedList<string>();
+
+            myLinkedList.addToLast("Jee");
+            myLinkedList.addToLast("Pee");
+            myLinkedList.addToLast("Lee");
+            myLinkedList.addToLast("Zee");
+
+            myLinkedList.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
