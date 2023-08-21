00a5d58cd8b0ebac0e8fd6a3b356f3e3a9064fb0
Sean <seansmaseola@gmail.com>
2023-08-21 10:13:29 +0200

    Generic methods class avtivity.

---
+++ Generics/ClassActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DataStructure.Generics
+{
+    internal class ClassActivity
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(compare<int>(5, 6));
+            Console.WriteLine(compare<double>(3.2, 3.2));
+            Console.WriteLine(compare<string>("Sean Maseola", "Maseola Sean"));
+            Console.WriteLine(getLength<int, string>(5, "kabelo"));
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+
+
+        public static bool compare<T>(T x, T y)
+        {
+            if (x.GetType() == typeof(string))
+            {
+                //return x.ToString().ToLower().Equals(y.ToString().ToLower());
+
+                return string.Compare(x.ToString(), y.ToString(), true) == 0;
+            }
+            return x.Equals(y);
+        }
+
+        public static bool getLength<T1, T2>(T1 num, T2 str)
+        {
+            return str.ToString().Length.Equals(num);
+        }
+
+        //public static bool compare(int x, int y)
+        //{
+
+        //    return x == y;
+        //}
+        //public static bool compare(double x, double y)
+        //{
+
+        //    return x == y;
+        //}
+        //public static bool compare(string x, string y)
+        //{
+
+        //    return x.Equals(y);
+        //}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,45 @@
-﻿namespace DataStructure
+﻿using System;
+
+
+namespace DataStructure
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            bool checkValue = true;
+            while (checkValue)
+            {
+                Console.WriteLine("Type two numbers: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine(compareInt(num1, num2));
+
+                Console.WriteLine("Want to try again(y/n)");
+                string che = Console.ReadLine();
+
+                if (che.ToLower() == "n")
+                {
+                    checkValue = false;
+                }
+
+
+                Console.ReadKey();
+            }
+
+        }
+
+        public static bool compareInt(int x, int y)
+        {
+            if (x == y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
